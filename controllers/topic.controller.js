c1b5430399218392cc8420ef2ac029195b251fbe
lucMarteau <lucmarteau@gmail.com>
2024-05-29 11:00:47 +0100

    task 4 get-api-articles-article_id has been completed

--- controllers/topic.controller.js
+++ controllers/topic.controller.js
@@ -1,4 +1,4 @@
-const { fetchTopics } = require("../models/topic.model");
+const { fetchTopics, fetchArticleId } = require("../models/topic.model");
 const apiEndpoints = require("../endpoints.json");
 
 const getTopics = (req, res, next) => {
@@ -12,5 +12,13 @@ const getApi = (req, res, next) => {
   res.status(200).send(apiEndpoints);
 
 };
+const getArticleId = (req, res, next) => {
+  const { article_id } = req.params;
+  fetchArticleId(article_id)
+    .then((article) => {
+      res.status(200).send({ article });
+    })
+    .catch(next);
+};
 
-module.exports = { getTopics, getApi };
+module.exports = { getTopics, getApi, getArticleId };
